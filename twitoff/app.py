2239384a1b6dc82f1de8746d4b87d2ff797af380
Sammy Lee <sammylee@Sammys-MacBook-Air.local>
2019-03-06 10:59:23 -0800

    added user.html, add_or_update_user(), and prdict.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,7 +2,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User, Tweet
-
+from .twitter import add_or_update_user
 
 
 def create_app():
@@ -10,13 +10,33 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['FLASK_ENV'] = config('FLASK_ENV')
+    # app.config['FLASK_ENV'] = config('FLASK_ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
     	users = User.query.all()
     	return render_template('base.html', title='Home', users=users)
+
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None):
+        message = ''
+        #import pdb; pdb.set_trace()
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+            
+        except Exception as e:
+            message = 'Error adding {}:{}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+            message=message)
+
 
     @app.route('/reset')
     def reset():
