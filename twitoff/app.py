07985918d05d8d3bde19bbe05de9772c86a66fd1
Sammy Lee <sammylee@Sammys-MacBook-Air.local>
2019-03-13 15:40:04 -0700

    added make_predict() for Appraisers-BFF

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,9 +1,16 @@
 """Main application and routing logic for TwitOff."""
-from decouple import config
 from flask import Flask, render_template, request
+from decouple import config
 from .models import DB, User, Tweet
 from .twitter import add_or_update_user
 from .predict import predict_user
+
+# Imports for make_predict()
+from flask import abort, jsonify, request
+import os
+import pickle
+import numpy as np
+
 
 
 def create_app():
@@ -13,6 +20,10 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # app.config['FLASK_ENV'] = config('FLASK_ENV')
     DB.init_app(app)
+
+    regressor = pickle.load(open(
+        os.path.join('pkl_objects',
+            'regressor.pkl'), 'rb'))
 
     @app.route('/')
     def root():
@@ -63,6 +74,19 @@ def create_app():
                                     tweeted=tweeted,
                                     prediction=y,
                                     probability=round(proba*100, 2))
+
+    
+
+    @app.route('/api', methods=['POST'])
+    def make_predict():
+        data = request.get_json(force=True)
+        predict_request = [data['beds'], data['baths'], data['square_feet'], data['lot_size'], data['hoa_per_month'], data['property_types'], data['property_age'], data['zip_codes']]
+        predict_request = np.array(predict_request)
+        y_hat = regressor.predict([predict_request])
+        y_hat = np.round(y_hat)
+        output = y_hat[0]
+        return jsonify(results=output)
+
 
 
     @app.route('/reset')
